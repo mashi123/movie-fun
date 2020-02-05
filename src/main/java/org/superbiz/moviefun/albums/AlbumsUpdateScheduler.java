b2bf4676b9af46fa09a4c7d8bb5e46baf7336b3c
Tomohiro Mashiyama <22511909+mashi123@users.noreply.github.com>
2020-02-05 16:30:19 +0900

    Background Jobs using AMQP

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.sql.DataSource;
 
-@Configuration
+// @Configuration
 @EnableAsync
 @EnableScheduling
 public class AlbumsUpdateScheduler {
